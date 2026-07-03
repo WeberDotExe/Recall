d18177e83ca9cc81486d584f6b677c46901f905a
WeberDotExe <ctaufiquee786@gmail.com>
2026-07-03 23:32:18 +0530

    aaa

--- frontend/src/components/auth/PublicRoute.jsx
+++ frontend/src/components/auth/PublicRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import useAuth from "../../hooks/UseAuth";
+import useAuth from "../../hooks/UseAuth.js";
 
 function PublicRoute() {
   const { accessToken } = useAuth();
